a3672b91be9e7042c913ad173ee23fec0f1b600d
Karim360Z <karim1zebian@gmail.com>
2022-08-17 13:01:27 +0300

    UI + Firebase

--- App.js
+++ App.js
@@ -5,6 +5,15 @@ import AppStackScreen from './src/stack/StackScreen'
 import { UserProvider } from './src/context/UserContext'
 import { FirebaseProvider } from './src/context/FirebaseContext'
 
+//Art Gallery Application
+//UI:
+//1-Login/Signup Screens
+//2-Home Screen to view and interact with posts
+//3-Posting Screen
+//4-Art Store Screen
+//5-Cart Screen
+//6-Profile Screen
+
 export default App = () => {
   return (
     <FirebaseProvider>
